24e1ee938bfa595ae207a2f1a6c5e06d97af059e
Mrudhula <mrudhulashrimanikantan@gmail.com>
2025-01-17 15:02:17 +0530

    mongodb

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,7 +1,27 @@
+import User from '../models/userModel.js';
 export const signup = async(req, res) =>{
     try{
         const {fullName, username, password, confirmPassword, gender}= req.body;
-    }
+        if(password!==confirmPassword){
+            return res.status(400).json({error:"Passwords didn't match"});
+        }
+        const user=await User.findOne({username});
+        if(user){
+            return res.status(400).json({error:"Username already exists"});
+        }
+        //Hash Password Here
+         //https://avatar-placeholder.iran.liara.run/
+         const boyProfilePic=`https://avatar.iran.liara.run/public/boy?username=${username}`
+         const girlProfilePic=`https://avatar.iran.liara.run/public/girl?username=${username}`
+         const newUser=new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender==="male" ? boyProfilePic : girlProfilePic
+         })
+
+    }   
     catch(error){
 
     }
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from './routes/authroutes.js'
+import authRoutes from './routes/authroutes.js';
+import mongoose from 'mongoose'
+import connectToMongoDb from "./db/connectToMongoDb.js";
 const app=express();
-dotenv.config();
 const PORT= process.env.PORT || 5000;
-app.get("/",(req, res)=>{
-    //root route http://localhost:5000/
-    res.send("Hello, World!!");
-});
+dotenv.config();
+
+app.use(express.json()); //to parse the incoming requests with json payloads
 app.use("/api/auth",authRoutes)
-app.listen(PORT,() =>console.log(`Server is running on the port ${PORT}`));
+
+
+
+// app.get("/",(req, res)=>{
+//     //root route http://localhost:5000/
+//     res.send("Hello, World!!");
+// });
+
+app.listen(PORT,() =>{
+    connectToMongoDb();
+    console.log(`Server is running on the port ${PORT}`);
+   
+});
+// console.log(process.env.MONGO_URI);
